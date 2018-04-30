33bb9eecfbc664434918de0b470c2f0b05ee2ede
jwson <hellonun91@gmail.com>
2018-04-30 15:21:21 +0900

    thymeleaf 추가

--- src/main/java/com/example/demo/HelloRestController.java
+++ src/main/java/com/example/demo/HelloRestController.java
@@ -1,14 +1,31 @@
 package com.example.demo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+import com.example.demo.dao.Car;
+
+@Controller
 public class HelloRestController {
 	
 	@RequestMapping("/")
+	@ResponseBody
 	public String index() {
 		// hello world를 찍어보자! test
 		return "hello world";
+	}
+	
+	@RequestMapping("/hello")
+	public String hello(Model model) {
+		// hello.html을 호출할 것
+		
+		Car car = new Car();
+		car.setColor("blue");
+		car.setDoor(4);
+		
+		model.addAttribute("car", car);
+		return "hello";
 	}
 }
---
+++ src/main/java/com/example/demo/dao/Car.java
@@ -0,0 +1,20 @@
+package com.example.demo.dao;
+
+public class Car {
+	
+	public int door;
+	public String color;
+
+	public int getDoor() {
+		return door;
+	}
+	public void setDoor(int door) {
+		this.door = door;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
