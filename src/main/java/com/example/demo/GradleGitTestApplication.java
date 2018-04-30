b37623e5ef9f5703ba368d31801c79e1429b9872
jwson <hellonun91@gmail.com>
2018-04-30 10:44:29 +0900

    gitignore 추가 및 first commit

---
+++ src/main/java/com/example/demo/GradleGitTestApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleGitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleGitTestApplication.class, args);
+	}
+}
