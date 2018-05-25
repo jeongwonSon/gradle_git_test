c893f46b625a7e297670c966393d0a18dfbe7432
jwson <hellonun91@gmail.com>
2018-05-25 12:21:51 +0900

    ErrorConfig.java 임시로 추가
    - 사용은 안하고 주석 처리로 해놨음
    - 따로 html 추가해야 함(403,404, 500등 에러에 맞게)

---
+++ src/main/java/com/example/demo/config/ErrorConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.config;
+
+import org.springframework.boot.web.server.ErrorPage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+
+@Configuration
+public class ErrorConfig {
+  
+//  @Bean
+//  public EmbeddedServletContainerCustomizer containerCustomizer() {
+//    return new EmbeddedServletContainerCustomizer() {
+// 
+//      @Override
+//      public void customize(ConfigurableEmbeddedServletContainer container) {
+//        ErrorPage error403Page = new ErrorPage(HttpStatus.FORBIDDEN, "/403.html");
+//        ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+//        ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+// 
+//        container.addErrorPages(error403Page, error404Page, error500Page);
+//      }
+//    };
+//  }
+
+}
